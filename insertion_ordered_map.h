7ef433ef77bfeb80afb2a755ab589d054ed6e8f9
cezary <cezary.bednarz@students.mimuw.edu.pl>
2019-12-20 21:06:53 +0100

    added lookup_error body

--- insertion_ordered_map.h
+++ insertion_ordered_map.h
@@ -13,35 +13,27 @@ template <class T>
 class CowPtr {
 public:
     typedef std::shared_ptr<T> RefPtr;
-
 private:
     RefPtr m_sp;
-
     void detach() {
         T* tmp = m_sp.get();
         if(!( tmp == 0 || m_sp.unique())) {
             m_sp = RefPtr(new T(*tmp));
         }
     }
-
 public:
     CowPtr(T* t) : m_sp(t) {}
-
     CowPtr(const RefPtr& refptr) : m_sp(refptr) {}
-
     const T& operator*() const {
         return *m_sp;
     }
-
     T& operator*() {
         detach();
         return *m_sp;
     }
-
     const T* operator->() const {
         return m_sp.operator->();
     }
-
     T* operator->() {
         detach();
         return m_sp.operator->();
@@ -107,15 +99,14 @@ public:
             return iterator(insertion_ordered_map::list_ptr->end());
         }
     };
-
-
-
 };
 
 class lookup_error : public std::exception {
 
 public:
-
+    const char* what() const throw() {
+    	return "lookup_error";
+    }
 };
 
 
