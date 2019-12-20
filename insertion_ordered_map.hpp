4344856c06da1596bc6a00ec6f85ff64e89fa062
cezary <cezary.bednarz@students.mimuw.edu.pl>
2019-12-20 16:46:38 +0100

    added class file

---
+++ insertion_ordered_map.hpp
@@ -0,0 +1,10 @@
+#ifndef __INSERTION_ORDERED_MAP__
+#define __INSERTION_ORDERED_MAP__
+
+template <class K, class V, class Hash = std::hash<K>>
+class insertion_ordered_map {
+  
+};
+
+
+#endif // __INSERTION_ORDERED_MAP__
