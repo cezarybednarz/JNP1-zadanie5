04d52afc249cf3618d42f90341f84167cd2472fa
Dawid Jamka <dawid@Dawids-MacBook-Pro.local>
2019-12-21 17:07:30 +0100

    pyyyk

--- insertion_ordered_map.h
+++ insertion_ordered_map.h
@@ -49,6 +49,7 @@ private:
 
     CowPtr<List> list_ptr;
     CowPtr<Map> map_ptr;
+    bool refered = false;
 
 public:
     insertion_ordered_map();
@@ -105,6 +106,117 @@ public:
     	return "lookup_error";
     }
 };
+
+template <class K, class V, class Hash>
+insertion_ordered_map<K, V, Hash>::insertion_ordered_map() {
+    list_ptr = CowPtr<List>(new List);
+    map_ptr = CowPtr<Map>(new Map);
+}
+
+template <class K, class V, class Hash>
+insertion_ordered_map<K, V, Hash>::insertion_ordered_map(insertion_ordered_map const &other) {
+    if(!other->refered) {
+        list_ptr = other.list_ptr;
+        map_ptr = other.map_ptr;
+    } else {
+        list_ptr = CowPtr(new List(*other.list_ptr));
+        map_ptr = CowPtr(new Map(*other.map_ptr));
+
+        for(typename List::iterator it = list_ptr->begin(); it != list_ptr->end(); ++it) {
+            map_ptr->at(it->first) = it;
+        }
+    }
+}
+template <class K, class V, class Hash>
+insertion_ordered_map<K, V, Hash>::insertion_ordered_map(insertion_ordered_map &&other) :
+    list_ptr(std::move(other.list_ptr)),
+    map_ptr(std::move(other.map_ptr)) {}
+
+template <class K, class V, class Hash>
+insertion_ordered_map<K, V, Hash> &insertion_ordered_map<K, V, Hash>::operator=(insertion_ordered_map other) {
+    map_ptr(other.map_ptr);
+    list_ptr(other.list_ptr);
+}
+
+template <class K, class V, class Hash>
+bool insertion_ordered_map<K, V, Hash>::insert(K const &k, V const &v) {
+    if(map_ptr->find(k) == map_ptr->end()) {
+        map_ptr->insert(k, list_ptr->push_back(std::make_pair(k, v)));
+        return true;
+    }
+    else {
+        V curr_v = map_ptr->at(k)->second;
+        list_ptr->erase(map_ptr->at(k));
+        map_ptr->at(k) = list_ptr->insert(list_ptr->push_back(std::make_pair(k, curr_v)));
+        return false;
+    }
+}
+
+template <class K, class V, class Hash>
+void insertion_ordered_map<K, V, Hash>::erase(K const &k){
+    list_ptr->erase(map_ptr->at(k));
+    map_ptr->erase(k);
+}
+
+template <class K, class V, class Hash>
+void merge(insertion_ordered_map<K, V, Hash> const &other) {
+    for(const auto &it : *other.list_ptr) {
+        insert(it->first, it->second);
+    }
+}
+
+template <class K, class V, class Hash>
+V &insertion_ordered_map<K, V, Hash>::at(K const &k) {
+    refered = true;
+    return list_ptr->find(map_ptr->at(k))->second;
+}
+
+template <class K, class V, class Hash>
+V const &insertion_ordered_map<K, V, Hash>::at(K const &k) const {
+    return list_ptr->find(map_ptr->at(k))->second;
+}
+
+template <class K, class V, class Hash>
+V &insertion_ordered_map<K, V, Hash>::operator[](K const &k) {
+    refered = true;
+
+    if(map_ptr->find(k) == map_ptr->end())
+        map_ptr->insert(k, V());
+
+    return list_ptr->find(map_ptr->at(k))->second;
+}
+
+template <class K, class V, class Hash>
+size_t insertion_ordered_map<K, V, Hash>::size() const {
+    return list_ptr->size();
+}
+
+template <class K, class V, class Hash>
+bool insertion_ordered_map<K, V, Hash>::empty() const {
+    return list_ptr->empty();
+}
+
+template <class K, class V, class Hash>
+void insertion_ordered_map<K, V, Hash>::clear() {
+    list_ptr->clear();
+    map_ptr->clear();
+    refered = false;
+}
+
+template <class K, class V, class Hash>
+bool insertion_ordered_map<K, V, Hash>::contains(K const &k) const {
+    return map_ptr->find(k) != map_ptr->end();
+}
+
+template <class K, class V, class Hash>
+typename insertion_ordered_map<K, V, Hash>::iterator &insertion_ordered_map<K, V, Hash>::begin() {
+    return insertion_ordered_map::iterator(list_ptr->begin());
+}
+
+template <class K, class V, class Hash>
+typename insertion_ordered_map<K, V, Hash>::iterator &insertion_ordered_map<K, V, Hash>::end() {
+    return insertion_ordered_map::iterator(list_ptr->end());
+}
 
 
 #endif // __INSERTION_ORDERED_MAP__
